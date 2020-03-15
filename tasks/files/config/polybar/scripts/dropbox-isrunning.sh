1e83409f0f573fd1ed437209e88dc88a1ec16c58
Diego Rubin <rubin.diego@gmail.com>
2020-03-15 18:13:13 -0300

    fix dropbox monitor

--- tasks/files/config/polybar/scripts/dropbox-isrunning.sh
+++ tasks/files/config/polybar/scripts/dropbox-isrunning.sh
@@ -1,18 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-case "$1" in
-    --toggle)
-        if [ "$(pgrep dropbox)" ]; then
-            pkill -f dropbox
-        else
-            dropbox &
-        fi
-        ;;
-    *)
-        if [ "$(pgrep dropbox)" ]; then
-            echo ""
-        else
-            echo ""
-        fi
-        ;;
-esac
+dropbox=$(ps aux | grep dropbox | grep -v "grep")
+
+echo "$dropbox"
+
+if [ -z "$dropbox" ]; then
+    echo ""
+else
+    echo ""
+fi

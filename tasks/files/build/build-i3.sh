49cee4ceb139b9f1104149940bf256c050a12a2d
Diego Rubin <contact@diegorubin.dev>
2020-08-21 04:53:27 -0300

    tests for fedora 32

---
+++ tasks/files/build/build-i3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /tmp/i3
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install

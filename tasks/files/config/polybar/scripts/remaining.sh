9e279eacffafb7f88321758c1eba236ab9bdfd90
Diego Rubin <contact@diegorubin.dev>
2020-11-18 08:28:50 -0300

    updating remaining script

--- tasks/files/config/polybar/scripts/remaining.sh
+++ tasks/files/config/polybar/scripts/remaining.sh
@@ -4,5 +4,9 @@ remaining=`head -n 1 ~/.remaining`
 if [ -z "$remaining" ]; then
   echo "XD"
 else
-  echo "  $remaining"
+  if [[ $remaining == *"-"* ]]; then
+    echo "%{F#f00}%{F-} $remaining"
+  else
+    echo "%{F#0f0}%{F-} $remaining"
+  fi
 fi

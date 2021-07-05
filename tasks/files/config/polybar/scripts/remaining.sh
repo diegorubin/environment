fff0d76c79f1b541fe8e04e195b446a32ea2a61b
Diego Rubin <contact@diegorubin.dev>
2021-07-05 12:57:35 -0300

    adding plans summary

--- tasks/files/config/polybar/scripts/remaining.sh
+++ tasks/files/config/polybar/scripts/remaining.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 remaining=`golem workday remaining`
+plans=`golem plans summary`
 if [ -z "$remaining" ]; then
   echo "XD"
 else
   if [[ $remaining == *"-"* ]]; then
-    echo "%{F#f00}%{F-} $remaining"
+    echo "%{F#f00}%{F-} ($plans) $remaining"
   else
-    echo "%{F#0f0}%{F-} $remaining"
+    echo "%{F#0f0}%{F-} ($plans) $remaining "
   fi
 fi

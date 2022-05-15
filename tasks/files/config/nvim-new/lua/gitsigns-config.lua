848c61fc34e92eb658eb89a31261165444c3c479
Diego Rubin <contact@diegorubin.dev>
2022-05-15 17:05:06 -0300

    updating gitsings configuration

--- tasks/files/config/nvim-new/lua/gitsigns-config.lua
+++ tasks/files/config/nvim-new/lua/gitsigns-config.lua
@@ -60,14 +60,16 @@ require('gitsigns').setup {
         ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
     },
-    watch_index = {interval = 1000, follow_files = true},
-    current_line_blame = false,
-    current_line_blame_delay = 1000,
-    current_line_blame_position = 'eol',
+    watch_gitdir = {interval = 1000, follow_files = true},
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+    },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
     word_diff = false,
-    use_decoration_api = true,
-    use_internal_diff = true -- If luajit is present
 }

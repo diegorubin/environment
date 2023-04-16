bee641fc22f7d405d7219e849c4e2eb76bbcd891
Diego Rubin <contact@diegorubin.dev>
2023-04-16 15:19:37 -0300

    changing lua ls

--- tasks/files/config/nvim/lua/languages-configs.lua
+++ tasks/files/config/nvim/lua/languages-configs.lua
@@ -8,32 +8,7 @@ require'lspconfig'.pyright.setup {}
 require'lspconfig'.denols.setup {}
 
 -- lua configuration
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {
-        "/opt/lua-language-server/bin/Linux/lua-language-server", "-E",
-        "/opt/lua-language-server/main.lua"
-    },
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true)
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {enable = false}
-        }
-    }
-}
+require'lspconfig'.lua_ls.setup {}
 
 require"lsp_signature".setup()
 
